4a442c26e55b486ba58bf668697c1dca3e2bf78a
agui <167501331+isntgui@users.noreply.github.com>
2025-02-21 13:07:01 -0400

    Update sieve.cpp

--- math/sieve.cpp
+++ math/sieve.cpp
@@ -7,9 +7,10 @@ int primo[maxn];
 
 void crivo(int n){
     memset(primo, -1, sizeof(primo));
+    primo[0] = primo[1] = 0;
     for(int i = 2; i <= n; i++){
         if(primo[i] == -1){
-            primo[i] = 1;
+            primo[i] = i;
             for(int j = i + i; j <= n; j += i){
                 primo[j] = 0;
             }
