e68effa4eae41d4450f726ece1ca84a7aa9197eb
agui <167501331+isntgui@users.noreply.github.com>
2025-02-27 21:09:27 -0400

    Update segtree.cpp

--- data structure/segtree.cpp
+++ data structure/segtree.cpp
@@ -1,10 +1,12 @@
+
 #include <bits/stdc++.h>
 using namespace std;
 
+#define int long long
+
 const int mxN=400;
 
-vector<int> a;
-int tree[4*mxN];
+int tree[4*mxN], n, a[mxn];
 
 int build(int node, int l, int r) {
 	if(l==r) return tree[node] = a[l];
@@ -20,7 +22,7 @@ int query(int node, int l, int r, int a, int b) {
 }
 
 void update(int node, int l, int r, int i, int x) {
-	if(i<l||r<i) return;
+	if(x<l||r<i) return;
 	if(l==r) {
 		tree[node] = x;
 		return;
@@ -31,14 +33,19 @@ void update(int node, int l, int r, int i, int x) {
 	tree[node] = tree[2*node] + tree[2*node+1];
 }
 
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	cout.tie(0);
-	a = { 0, 1, 3, 5, -2, 3 };
-	int n = a.size();
-	build(1, 0, n-1);
-	cout << "Sum of values in range 0-4 are: " << query(1, 0, n - 1, 0, 4) << "\n";
-	update(1, 0, n - 1, 1, 100);
-	cout << "sum of value in range 1-3 are: " << query(1, 0, n - 1, 1, 3) << "\n";
+int bs_st(int node, int l, int r, int k) {
+    int s=0, p=0;
+    for(int i=30; i>=0; --i) {
+        if(p+(1<<i) <= n && s+query(1, 1, n, p+1, p+(1<<i)) <= k)
+            s+=query(1, 1, n, p+1, p+(1<<i)), p+=(1<<i);
+    }
+    return p;
+}
+
+int32_t main() {
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    
+    ;
 }
