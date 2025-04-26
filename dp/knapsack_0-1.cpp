67e5a2054f03683265f98c5ab6c581b6a3ad36d8
agui <167501331+isntgui@users.noreply.github.com>
2025-04-25 20:33:26 -0400

    Update knapsack_0-1.cpp

--- dp/knapsack_0-1.cpp
+++ dp/knapsack_0-1.cpp
@@ -1,21 +1,17 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
-const int mxX=300, mxN=1e3+1;
-int t, x, p[mxX], v[mxN], dp[mxN];
+
+const int mxn=100, mxX=650;
+int n, w, p[mxn], v[mxn], dp[mxX], ind=0;
 
 int main() {
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	int g;
-	cin >> g;
-	for (int tt=1; tt<=g; ++tt) {
-		cout << "Galho " << tt << ":\n";
-		memset(dp, 0, sizeof(dp));
-		cin >> t >> x;
-		for (int i=0; i<t; ++i)
-			cin >> v[i] >> p[i];
-		for (int i=0; i<t; ++i)
-			for (int j=x; j>=p[i]; --j)
-				dp[j] = max(dp[j], dp[j-p[i]]+v[i]);
-		cout << "Numero total de enfeites: " << dp[x] << "\n\n";	
-	}
+	
+	cin >> n >> w;
+	for(int i=0; i<n; ++i)
+		cin >> p[i] >> v[i];
+	for(int i=0; i<n; ++i)
+		for(int j=w; j>=p[i]; --j)
+			dp[j] = max(dp[j], dp[j-p[i]]+v[i]);
+	cout << dp[w] << "\n";
 }
