f1ac8961cfa941962c0c1f24f63540389040de9d
agui <167501331+isntgui@users.noreply.github.com>
2025-04-25 20:25:26 -0400

    Update kadane.cpp

--- dp/kadane.cpp
+++ dp/kadane.cpp
@@ -1,34 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std;
-
+	
 int kadane(vector<int> v) {
-    int mu = 0, ml = 0;
-    for (auto i : v) {
-        ml += i;
-        if (ml > mu)
-            mu = ml;
-        if (ml < 0)
-            ml = 0;
-    }
-    return mu;
+	int mu = 0, ml = 0;
+	for (auto i : v) {
+		ml += i;
+		if (ml > mu)
+			mu = ml;
+		if (ml < 0)
+			ml = 0;
+	}
+	return mu;
 }
-
+	
 int main() {
 	ios::sync_with_stdio(0);
-  cin.tie(nullptr);
-  cout.tie(nullptr);
-
-  int n;
-  while (cin >> n) {
-      int t;
-      vector<int> vi;
-      cin >> t;
-      while (n--) {
-          int num;
-          cin >> num;
-          num -= t;
-          vi.push_back(num);
-      }
-      cout << kadane(vi) << "\n";
-  }
+	cin.tie(nullptr);
+	cout.tie(nullptr);
+	
+	;
 }
