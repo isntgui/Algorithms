8e57a3fb0e1aaf3f1b8b52352fa55edaf6127374
agui <167501331+isntgui@users.noreply.github.com>
2025-04-25 20:29:38 -0400

    Update knapsack.cpp

--- dp/knapsack.cpp
+++ dp/knapsack.cpp
@@ -2,22 +2,16 @@
 using namespace std;
 
 const int mxn=100, mxX=650;
-int n, t, p[mxn], v[mxn], dp[mxX], ind=0;
+int n, w, p[mxn], v[mxn], dp[mxX], ind=0;
 
 int main() {
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-	while (cin >> n >> t && n) {
-		cout << "Instancia " << ++ind << "\n";
-		memset(dp, 0, sizeof(dp));
-		for (int i=0; i<n; ++i) {
-			cin >> p[i] >> v[i];
-		}
-		for (int i=0; i<=t; ++i) {
-			for (int j=0; j<=n; ++j) {
-				if (p[j] <= i) 
-					dp[i] = max(dp[i], dp[i-p[j]] + v[j]);
-			}
-		}
-		cout << dp[t] << "\n\n";
-	}
+	
+	cin >> n >> w;
+	for(int i=0; i<n; ++i)
+		cin >> p[i] >> v[i];
+	for(int i=0; i<n; ++i)
+		for(int j=w; j>=p[i]; --j)
+			dp[j] = max(dp[j], dp[j-p[i]]+v[i]);
+	cout << dp[w] << "\n";
 }
