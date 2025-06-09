d2874801d3c24dec938f529af47172c42eda56f5
agui <167501331+isntgui@users.noreply.github.com>
2025-06-08 21:11:16 -0400

    Update sieve.cpp

--- math/sieve.cpp
+++ math/sieve.cpp
@@ -1,19 +1,52 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int maxn = 100010;
+const int mxn = 1e6+10;
 
-int primo[maxn]; 
+int primo[mxn];
 
-void crivo(int n){
-    memset(primo, -1, sizeof(primo));
+void eh_primo() {
+    fill(primo, primo+mxn, -1);
     primo[0] = primo[1] = 0;
-    for(int i = 2; i <= n; i++){
-        if(primo[i] == -1){
+    for(int i=2; i<mxn; i++) {
+        if(primo[i]==-1) {
             primo[i] = i;
-            for(int j = i + i; j <= n; j += i){
+            for(int j=2*i; j<mxn; j+=i) {
                 primo[j] = 0;
             }
         }
     }
+}
+
+void fat_crivo(){
+    for(int i=2; i<mxn; i++) {
+        if(primo[i]==0) {
+            for(int j=i; j<mxn; j+=i) {
+                if(primo[j]==0) {
+                    primo[j] = i;
+                }
+            }
+        }
+    }
+}
+
+vector<int> fatorar(int x) {
+    vector<int> div;
+    while(x>1) {
+        div.push_back(primo[x]);
+        x/=primo[x];
+    }
+    return div;
+}
+
+int main() {
+    // fat_crivo();
+    // eh_primo();
+    // int n;
+    // cin >> n;
+    // vector<int> ans = fatorar(n);
+    // for(int i=0; i<(int)ans.size(); i++) {
+    //     cout << ans[i] << " ";
+    // }
+    // cout << primo[n];
 }
