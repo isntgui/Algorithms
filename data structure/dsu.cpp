50390ae79bcc3571f2250989df62e7c2e95c28f3
agui <167501331+isntgui@users.noreply.github.com>
2025-07-03 15:56:46 -0400

    Update dsu.cpp

--- data structure/dsu.cpp
+++ data structure/dsu.cpp
@@ -1,44 +1,48 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-const int mxn = 1e4+10;
+#define int int64_t
 
-int parent[mxn], height[mxn];
-
-int find(int i) {
-    return (i==parent[i]?i:parent[i] = find(parent[i]));
-}
-
-void join(int i, int j) {
-    int x = find(i), y = find(j);
-    if(!(x==y)) {
-        if(height[x]>height[y])
-            parent[y] = x;
-        else {
-            parent[x] = y;
-            if(height[x]==height[y])
-                ++height[y];
+class UF {
+    private:
+        vector<int> peso, pai, size;
+    public:
+        UF(int n) {
+            pai.resize(n+1);
+            peso.resize(n+1);
+            size.resize(n+1, 1);
+            iota(pai.begin(), pai.end(), 0);
         }
-    }
-}
 
-int main() {
-    ios::sync_with_stdio(0);
+        int find(int i) {
+            return pai[i] = (pai[i]==i?i:find(pai[i]));
+        }
+
+        // Union by rank & size
+        void joint(int i, int j) {
+            i = find(i), j = find(j);
+            if(i==j) return ;
+            if(peso[i]>peso[j]) {
+                pai[j] = i;
+                size[i]+=size[j];
+            } else if(peso[i]<peso[j]) {
+                pai[i] = j;
+                size[j]+=size[i];
+            }  else {
+                pai[i] = j;
+                size[i]+=size[j];
+                peso[j]++;
+            }
+        }
+
+        int set_size(int i) {
+            return size[find(i)];
+        }
+};
+
+int32_t main() {
+    ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    cout.tie(nullptr);
 
-    memset(height, 0, sizeof(height));
-    iota(parent, parent+mxn, 0); // inicializa o vetor em sequencia, dependendo do intervalo
-
-    int _max, n, m;
-    while(cin >> n >> m) {
-        _max = max(_max, max(n, m));
-        join(n, m);
-    }
-
-    for(int i=0; i<=_max; ++i)
-        cout << "Antecessor[" << i << "] = " << parent[i] << ".\n";
-    cout << "\n";
-    for(int i=0; i<=_max; ++i)
-        cout << "Altura[" << i << "] = " << height[i] << ".\n";
+    ;
 }
