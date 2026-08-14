623e3c6c9b4641446197cbe8a6081d4b28041df3
agui <167501331+isntgui@users.noreply.github.com>
2026-08-14 16:54:03 -0400

    Update knapsack_0-1.cpp

--- dp/knapsack_0-1.cpp
+++ dp/knapsack_0-1.cpp
@@ -15,3 +15,50 @@ int main() {
 			dp[j] = max(dp[j], dp[j-p[i]]+v[i]);
 	cout << dp[w] << "\n";
 }
+
+/*
+Obs: Quando o valor é muito alto, pode ser possível utilizar um dp 'inversa', como a dp é pelo máximo de valor, essa dp calcula 
+o mínimo de peso para fazer a soma, isso funcionando quando W >= Soma_dos_valores
+	
+#include <bits/stdc++.h>
+using namespace std;
+
+#if __has_include("debugging.h")
+#include "debugging.h"
+#endif
+
+#define int long long int
+
+#define all(a) a.begin(), a.end()
+#define sz(a) (int)a.size()
+#define fr first
+#define sc second
+
+constexpr int INF = 1e18 + 10;
+
+int32_t main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, w;
+    cin >> n >> w;
+    vector<int> a(n), b(n);
+    int soma = 0;
+    for (int i = 0; i < n; i++)
+        cin >> a[i] >> b[i], soma += b[i];
+    vector<int> dp(soma + 1, INF);
+    dp[0] = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = soma; j >= b[i]; j--) {
+            dp[j] = min(dp[j], dp[j - b[i]] + a[i]);
+        }
+    }
+    for (int i = soma; i >= 0; i--) {
+        if (dp[i] <= w) {
+            cout << i << "\n";
+            return 0;
+        }
+    }
+}
+
+*/
