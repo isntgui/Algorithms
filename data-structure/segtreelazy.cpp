bdb5ddce2e4d83c10e10a60d401a8b8f99bd3bf7
agui <souzaagnello70@gmail.com>
2026-08-27 09:58:34 -0400

    Refatoração do código de SegTree com Lazy Propagation

--- data-structure/segtreelazy.cpp
+++ data-structure/segtreelazy.cpp
@@ -1,52 +1,76 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-const int mxN = 1e5;
+struct SegmentTree {
+    vector<int> st, lz, a;
+	const int NEUTRAL_ELEMENT = 0;
+    int n;
 
-int lazy[4*mxN], st[4*mxN], a[mxN+1];
-
-void bld(int node, int l, int r) {
-    if(l==r) {
-        st[node] = a[l];
-        return;
+    SegmentTree(vector<int>& v) : n((int)v.size()), a(v) {
+        st.assign(4 * (n + 1) + 5, 0);
+		lz.assign(4 * (n + 1) + 5, 0);
+        build(1, 1, n);
     }
-    int m = (l+r)/2;
-    bld(2*node, l, m);
-    bld(2*node+1, m+1, r);
-    st[node] = max(st[2*node], st[2*node+1]);
-}
 
-void unlazy(int node, int l, int r) {
-	if(lazy[node]==0) return ;
-	st[node] += (r-l+1) * lazy[node];
-	if(l!=r) {
-		lazy[2*node] = lazy[node];
-		lazy[2*node+1] = lazy[node];
+    int merge(int a, int b) {
+        return a + b;
+    }
+
+	void unlazy(int node, int l, int r) {
+		if (lz[node] == 0)
+			return ;
+		st[node] += (r - l + 1) * lz[node];
+		if (l != r) {
+			lz[2 * node] = lz[node];
+			lz[2 * node + 1] = lz[node];
+		}
+		lz[node] = 0;
 	}
-	lazy[node] = 0;
-}
 
-void upd(int node, int l, int r, int i, int j, int v) {
-	unlazy(node, l, r);
-	if(l>j||r<i) return;
-	if(l>=i&&r<=j) {
-		lazy[node] = v;
+    void build(int node, int l, int r) {
+        if (l == r) {
+            st[node] = a[l];
+            return;
+        }
+        int mid = (l + r) / 2;
+        build(2 * node, l, mid);
+        build(2 * node + 1, mid + 1, r);
+        st[node] = merge(st[2 * node], st[2 * node + 1]);
+    }
+
+    void update(int node, int l, int r, int i, int j, int val) {
 		unlazy(node, l, r);
-		return;
-	}
-	int m = (l+r)/2;
-	upd(2*node, l, m, i, j, v);
-	upd(2*node+1, m+1, r, i, j, v);
-	st[node] = max(st[2*node], st[2*node+1]);
-}
+        if (r < i || l > j)
+            return;
+		if (i <= l && r <= j) {
+			lz[node] += val;
+			unlazy(node, l, r);
+			return ;
+		}
+        int mid = (l + r) / 2;
+		update(2 * node, l, mid, i, j, val);
+		update(2 * node + 1, mid + 1, r, i, j, val);
+        st[node] = merge(st[2 * node], st[2 * node + 1]);
+    }
 
-int qry(int node, int l, int r, int a, int b) {
-	unlazy(node, l, r);
-	if(l>b||r<a) return 0;
-	if(a<=l&&b>=r) return st[node];
-	int m = (l+r)/2;
-	return max(qry(2*node, l, m, a, b), qry(2*node+1, m+1, r, a, b));
-}
+    int query(int node, int l, int r, int i, int j) {
+		if (j < l || i > r)
+            return NEUTRAL_ELEMENT;
+		unlazy(node, l, r);
+        if (i <= l && j >= r)
+            return st[node];
+        int mid = (l + r) / 2;
+        return merge(query(2 * node, l, mid, i, j), query(2 * node + 1, mid + 1, r, i, j));
+    }
+
+    void update(int a, int b, int v) {
+        update(1, 1, n, a, b, v);
+    }
+
+    int query(int a, int b) {
+        return query(1, 1, n, a, b);
+    }
+};
 
 int main() {
 	ios::sync_with_stdio(0);
